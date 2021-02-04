a347591be1bb676cb0d5900deb2952fddd2d3e43
Anurag <anuragarwalkar@gmail.com>
2021-02-04 22:05:43 +0530

    added list methods

--- list.py
+++ list.py
@@ -8,3 +8,15 @@ names[0] = "Amina"
 print(names[0])
 
 print(names[0:3])
+
+# List methods
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+numbers.insert(0, -1)
+numbers.remove(3)
+is_there = 1 in numbers
+print(is_there)
+print(numbers)
+print(len(numbers))
+numbers.clear()
+print(numbers)
